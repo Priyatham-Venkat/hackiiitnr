a7ba0cb645fcb95bc93c2a446fe6a7a06e486ae0
sundarkumarakella <ask1997test@gmail.com>
2018-03-17 17:33:37 +0530

    Revert "Added pages"
    This reverts commit 00ecca03f03e2c247b87f41c969d5c57ac4555e4.

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,71 @@
 <?php
 session_start();
 
+?>
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>For My City :: Dashboard </title>
+	<link rel="stylesheet" type="text/css" href="css/dashboard.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
+
+
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<!-- <script src="js/dashboard.js"></script> -->
+</head>
+<body>
+<section id="sidebar"> 
+  <div class="white-label">
+  </div> 
+  <div id="sidebar-nav">   
+    <ul>
+      <li class="active"><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+      <li><a href="oldage.php"><i class="fa fa-calendar"></i>Old Age Home</a></li>
+       <li><a href="orphanage.php"><i class="fa fa-calendar"></i>Orphanages</a></li>
+      <li><a href="cleaning.php"><i class="fa fa-calendar"></i>Cleaning Campaign</a></li>
+      <li><a href="awareness.php"><i class="fa fa-calendar"></i>Awareness Campaign</a></li>
+      <li><a href="teaching.php"><i class="fa fa-calendar"></i>Teaching Campaign</a></li>
+      <li><a href="plantation.php"><i class="fa fa-calendar"></i>Plantation Programmes</a></li>
+      <li><a href="health.php"><i class="fa fa-calendar"></i>Health Drives</a></li>
+    </ul>
+  </div>
+</section>
+<section id="content">
+  <div id="header">
+    <div class="header-nav">
+      <div class="menu-button">
+      </div>
+      <div class="nav">
+        <ul>
+          <li class="nav-profile">
+            <div class="nav-profile-image">
+              <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+              <div class="nav-profile-name"><!-- <?php echo "$uname" ?> -->SUNDAR KUMAR</div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="content" id="dashboard-content">
+    <div class="content-header">
+    	<div class="row">
+    		<div class="col-md-9">
+      <h1>Dashboard</h1>
+      	</div>
+      	<div class="col-md-3" align="right">
+      		<a href="logout.php" class="btn btn-danger">LOG OUT</a>
+      	</div>
+      	</div>
+    </div>
+      gdhadghsagdhasgdhsadhsadhgdgdahf
+  </div>
+  </section>
+</body>
+</html>
